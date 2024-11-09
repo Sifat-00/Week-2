295cfe552c08aaccd5b22cf60f8c4bead2453c87
Sifat-00 <sifatbindoha123@gmail.com>
2024-11-09 00:13:27 -0800

    Update u_Qu.cpp

--- Week 2/Day 6/u_Qu.cpp
+++ Week 2/Day 6/u_Qu.cpp
@@ -1,10 +1,7 @@
 #include<bits/stdc++.h>
-#define ll long long int
 using namespace std;
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(NULL);
+int main()
+{
     int t;
     cin>>t;
     while(t--)
